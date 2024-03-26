e63d0f9fcc19c1a09d96f48e5f87ca473730aac6
Accoining <151427105+Accoining@users.noreply.github.com>
2024-03-26 22:29:34 +0800

    Create 十进制转十六进制.cpp

---
+++ 十进制转十六进制.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,num;
+    char dig[200];
+    cin>>num;
+    for(i=0;num>0;i++){
+        dig[i]=num%2;
+        num=num/2;
+    }
+    for(i=i-1;i>=0;i--){
+        if(dig[i]<10)
+            cout<<char(dig[i]+'0');
+        else
+            cout<<char(dig[i]+'a');
+    }
+    return 0;
+}
+        
